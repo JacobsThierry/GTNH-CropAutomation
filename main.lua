b8833cef93627b4408745e48a1a6659cb17f01da
Your Name <you@example.com>
2024-08-02 18:32:09 +0200

    woo

--- main.lua
+++ main.lua
@@ -196,7 +196,6 @@ local function checkChild(slot, crop)
             database.updateFarm(slot, scanner.scan())
             database.updateFarm(lowestParentScoreSlot, crop)
             updateDbInfos()
-            actions.placeCropStick(2)
          elseif isChildGoodEnough then
             -- keep
          else
